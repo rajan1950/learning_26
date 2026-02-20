028ff0c021b5ab7d0ebcd930d96a3dddf1f8756c
rajan1950 <rajandobariya6@gmail.com>
2026-02-20 21:39:54 +0530

    create NetflixHome component

--- src/components/NetflixHome.jsx
+++ src/components/NetflixHome.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const NetflixHome = () => {
   return (
     <div style={{ textAlign: "center" }}>
-      <h1>Netflix Home</h1>
+      <h1 style={{ color: "red" }}>Netflix Home</h1>
       <table className='table table-warning' style={{ marginLeft: "400px", width: "500px", marginTop: "50px" }}>
         <thead>
           <tr>
